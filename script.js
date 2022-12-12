fc0d104f2f23163e2706c565687994c76042c8af
Patrik200414 <martinpatrik14@gmail.com>
2022-12-12 20:59:41 +0100

    Makeing the logic of the decession maker
    I created a function whitch will decide who won
    added two parameters to it
    the two parameters are the other functions

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var play = true;
+var playStart = true;
+console.log(play(getComputerChoice(), playerSelection()));
 
 
 
 function getComputerChoice(){
     let choices = ['Rock', 'Paper', 'Scissors'];
-    return choices[Math.floor(Math.random() * (2-0)) + 0];
+    return choices[Math.floor(Math.random() * (3-0)) + 0];
 }
 
 function playerSelection(){
@@ -27,5 +28,48 @@ function playerSelection(){
     }
 }
 
-console.log(playerSelection());
+function play(getComputerChoice, playerSelection){
+    let computer = getComputerChoice;
+    let player = playerSelection;
+    if(player === 'Rock' && computer === 'Scissors'){
+        console.log(`You Won!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === 'Paper' && computer === 'Rock'){
+        console.log(`You Won!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === 'Scissors' && computer === 'Paper'){
+        console.log(`You Won!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === 'Scissors' && computer === 'Rock'){
+        console.log(`You Lost!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === 'Rock' && computer === 'Paper'){
+        console.log(`You Lost!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === 'Paper' && computer === 'Scissors'){
+        console.log(`You Lost!
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+    else if(player === null){
+        console.log('Unvaled parameter!');
+    }
+    else{
+        console.log(`You tied
+                    You: ${player}
+                    Computer: ${computer}`);
+    }
+}
+
+
 
