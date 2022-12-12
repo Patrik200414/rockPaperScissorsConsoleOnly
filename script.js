c341b21e66b6bce3606ba49af0c44f3674181a6b
Patrik200414 <martinpatrik14@gmail.com>
2022-12-12 16:50:32 +0100

    Make the player's choice function
    I made the player's choice function
    It's not case sensetive
    It's returns the value of your choice
    If the choice is not avaible it will reeturn the value of null

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * (2-0)) + 0];
 }
 
+function playerSelection(){
+    let playerChoice = prompt('Please enter your choice! (Rock, paper, scissors)');
+    let rock = /Rock/ig;
+    let paper = /Paper/ig;
+    let scissors = /scissors/ig;
+    
+    if(rock.test(playerChoice)){
+        return 'Rock';
+    }
+    else if(paper.test(playerChoice)){
+        return 'Paper';
+    }
+    else if(scissors.test(playerChoice)){
+        return 'Scissors';
+    }
+    else{
+        return null;
+    }
+}
+
+console.log(playerSelection());
+
